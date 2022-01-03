eecc308d9dcc052dfea47214a76db250d23987f8
Sovanaryth THORNG <sovanaryth.thorng@gmail.com>
2022-01-03 11:44:03 +0100

    Add all site sections

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget("./src/styles/tailwind.css");
-  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/images");
   return {
     dir: {
       input: "src",
