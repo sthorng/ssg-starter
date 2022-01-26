13ed71571a82ce0a80317a13b3c05fe33b2efb17
Sovanaryth THORNG <sovanaryth.thorng@gmail.com>
2022-01-26 23:28:09 +0100

    Add new sections energy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 500ms ease-in forwards",
+        fadeOut: "fadeOut 500ms ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 }
+        }
+      }
+    },
+  },
+  variants: {
+    animation: ["motion-safe"]
   },
   plugins: [
     require("@tailwindcss/forms"),
